713b6203184a5eb4c1cc10034366165a165ce549
myusernamedm <39332143+myusernamedm@users.noreply.github.com>
2024-04-27 11:40:27 +0200

    Move Azure local to GitHub

---
+++ UITestStringsLibrary/CrossPageSharedUITestStrings.cs
@@ -0,0 +1,382 @@
+﻿using System;
+
+namespace UITestStringsLibrary
+{
+    public class CrossPageSharedUITestStrings
+    {
+        public string EmailFieldRequiredValidationDeutsch()
+        {
+            string EmailFieldRequiredValidationDeutsch = "Das Feld Mailen ist erforderlich.";
+            return EmailFieldRequiredValidationDeutsch;
+        }
+
+        public string EmailFieldNotValidValidationDeutsch()
+        {
+            string EmailFieldNotValidValidationDeutsch = "Das Feld Mailen ist keine gültige E-Mail-Adresse.";
+            return EmailFieldNotValidValidationDeutsch;
+        }
+
+        public string PasswordFieldRequiredValidationDeutsch()
+        {
+            string PasswordFieldRequiredValidationDeutsch = "Das Feld Passwort ist erforderlich.";
+            return PasswordFieldRequiredValidationDeutsch;
+        }
+
+        public string PasswordFieldMustBeValidationDeutsch()
+        {
+            string PasswordFieldMustBeValidationDeutsch = "Das Passwort muss eine Zeichenfolge mit einer Mindestlänge von 6 und einer Höchstlänge von 100 sein.";
+            return PasswordFieldMustBeValidationDeutsch;
+        }
+
+        public string PasswordFieldsMustMatchValidationDeutsch()
+        {
+            string PasswordFieldsMustMatchValidationDeutsch = "'Passwort bestätigen' und 'Passwort' stimmen nicht überein.";
+            return PasswordFieldsMustMatchValidationDeutsch;
+        }
+
+        public string TestUserEmail()
+        {
+            string testEmail = "test@email.com";
+            return testEmail;
+        }
+
+        public string TestUserPassword()
+        {
+            string testPswd = "6pHAELTn4sB*SN%";
+            return testPswd;
+        }
+        public string Test1UserEmail()
+        {
+            string test1Email = "test1@email.com";
+            return test1Email;
+        }
+
+        public string Test1UserPassword()
+        {
+            string test1Pswd = "VBWNY55G-6.~7iZ";
+            return test1Pswd;
+        }
+        public string Test2UserEmail()
+        {
+            string test2Email = "test2@email.com";
+            return test2Email;
+        }
+        public string Test2UserPassword()
+        {
+            string test2Pswd = "Test1234#";
+            return test2Pswd;
+        }
+        public string Test3UserEmail()
+        {
+            string test3Email = "test3@email.com";
+            return test3Email;
+        }
+
+        public string Test3UserPassword()
+        {
+            string test3Pswd = "Test1234#";
+            return test3Pswd;
+        }
+        public string Test4UserEmail()
+        {
+            string test4Email = "test4@email.com";
+            return test4Email;
+        }
+
+        public string Test4UserPassword()
+        {
+            string test4Pswd = "Test1234";
+            return test4Pswd;
+        }
+        //DO NOT CHANGE THESE VALUES - test5User is being used for unit testing the PRODUCTION DATABASE
+        // See TestBlobs.cs
+        public string Test5UserEmail()
+        {
+            string test5Email = "test5@email.com";
+            return test5Email;
+        }
+        public string Test5UserPassword()
+        {
+            string test5Pswd = "Test1234";
+            return test5Pswd;
+        }
+        public string Test5UserId()
+        {
+            string test5Id = "aa8d3c98-010c-4f60-885e-af4cbae438b0";
+            return test5Id;
+        }
+        //DO NOT CHANGE THESE VALUES - test5User is being used for unit testing the PRODUCTION DATABASE
+        // See TestBlobs.cs
+
+        public string Co2icon1String()
+        {
+            string co2icon1 = "co2icon1.png";
+            return co2icon1;
+        }
+        public string Co2icon2String()
+        {
+            string co2icon2 = "co2icon2.png";
+            return co2icon2;
+        }
+        public string Co2icon3String()
+        {
+            string co2icon3 = "co2icon3.png";
+            return co2icon3;
+        }
+        public string Co2icon4String()
+        {
+            string co2icon4 = "co2icon4.png";
+            return co2icon4;
+        }
+        public string Co2icon5String()
+        {
+            string co2icon5 = "co2icon5.png";
+            return co2icon5;
+        }
+        public string rhinoicon1String()
+        {
+            string rhinoicon1 = "rhinoicon1.png";
+            return rhinoicon1;
+        }
+        public string rhinoicon2String()
+        {
+            string rhinoicon2 = "rhinoicon2.png";
+            return rhinoicon2;
+        }
+        public string rhinoicon3String()
+        {
+            string rhinoicon3 = "rhinoicon3.png";
+            return rhinoicon3;
+        }
+        public string rhinoicon4String()
+        {
+            string rhinoicon4 = "rhinoicon4.png";
+            return rhinoicon4;
+        }
+        public string rhinoicon5String()
+        {
+            string rhinoicon5 = "rhinoicon5.png";
+            return rhinoicon5;
+        }
+        public string treeicon1String()
+        {
+            string treeicon1 = "treeicon1.png";
+            return treeicon1;
+        }
+        public string treeicon2String()
+        {
+            string treeicon2 = "treeicon2.png";
+            return treeicon2;
+        }
+        public string treeicon3String()
+        {
+            string treeicon3 = "treeicon3.png";
+            return treeicon3;
+        }
+        public string treeicon4String()
+        {
+            string treeicon4 = "treeicon4.png";
+            return treeicon4;
+        }
+        public string treeicon5String()
+        {
+            string treeicon5 = "treeicon5.png";
+            return treeicon5;
+        }
+        public string GetMouseClickIconThumbnailUrlForReduceDeforestationPage()
+        {
+            string MouseClickIconThumbnailUrl = "/images/mousetap.png";
+            return MouseClickIconThumbnailUrl;
+        }
+        public string GetHandTapIconThumbnailUrlForReduceDeforestationPage()
+        {
+            string HandTapIconThumbnailUrl = "/images/handtap.png";
+            return HandTapIconThumbnailUrl;
+        }
+        public string Test7UserId()
+        {
+            string test7IdPswd = "2fde30bf-43c5-4db8-96e1-6c98ed898399";
+            return test7IdPswd;
+        }
+        public string Test22UserEmail()
+        {
+            string test22Email = "test22@email.com";
+            return test22Email;
+
+        }
+        public string TestCreateArticleSnippet1Char()
+        {
+            string value = "1";
+            return value;
+
+        }
+        public string TestCreateArticleSnippet300Chars()
+        {
+            string value = "A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1";
+            return value;
+
+        }
+        public string TestCreateArticlePara3000Chars()
+        {
+            string value = "A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1 A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1 A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1 A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1 A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1 A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1 A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1 A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1 A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet 1 A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article snippet A test article ";
+            return value;
+
+        }
+        public string TestCreateArticlePara49Chars()
+        {
+            string value = "cle snippet A test article snippet A test article";
+            return value;
+
+        }
+        public string TestCreateArticlePara50Chars()
+        {
+            string value = "cle snippet A test article snippet A test article ";
+            return value;
+        }
+        public string TestCreateArticlePara1RequiredValidation()
+        {
+            string value = "THE PARA 1 TEXT FIELD IS REQUIRED.";
+            return value;
+        }
+        public string TestCreateArticlePara1LengthValidation()
+        {
+            string value = "THE FIELD PARA 1 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 300 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara2LengthValidation()
+        {
+            string value = "THE FIELD PARA 2 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara3LengthValidation()
+        {
+            string value = "THE FIELD PARA 3 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara4LengthValidation()
+        {
+            string value = "THE FIELD PARA 4 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara5LengthValidation()
+        {
+            string value = "THE FIELD PARA 5 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara6LengthValidation()
+        {
+            string value = "THE FIELD PARA 6 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara7LengthValidation()
+        {
+            string value = "THE FIELD PARA 7 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara8LengthValidation()
+        {
+            string value = "THE FIELD PARA 8 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara9LengthValidation()
+        {
+            string value = "THE FIELD PARA 9 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara10LengthValidation()
+        {
+            string value = "THE FIELD PARA 10 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara11LengthValidation()
+        {
+            string value = "THE FIELD PARA 11 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara12LengthValidation()
+        {
+            string value = "THE FIELD PARA 12 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara13LengthValidation()
+        {
+            string value = "THE FIELD PARA 13 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara14LengthValidation()
+        {
+            string value = "THE FIELD PARA 14 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticlePara15LengthValidation()
+        {
+            string value = "THE FIELD PARA 15 TEXT MUST BE A STRING WITH A MINIMUM LENGTH OF 50 AND A MAXIMUM LENGTH OF 3000.";
+            return value;
+        }
+        public string TestCreateArticleTandCValidation()
+        {
+            string value = "THE TANDC FIELD MUST BE CHECKED IN ORDER TO CONTINUE.";
+            return value;
+        }
+        public string TestCreateArticleTitleImageValidationFieldValidJpg()
+        {
+            string value = "C:\\fakepath\\valid_jpg.jpg";
+            return value;
+        }
+        public string TestCreateArticleTitleImageValidationFieldValidPng()
+        {
+            string value = "C:\\fakepath\\valid_png.png";
+            return value;
+        }
+        public string TestCreateArticleVideoValidation()
+        {
+            string value = "ENTRY MUST BE A VALID URL FOR A YOUTUBE VIDEO";
+            return value;
+        }
+        public string TestCreateArticleVideoUrlType1()
+        {
+            string value = "https://www.youtube.com/watch?app=desktop&v=ZY6RfVY-Zl0";
+            return value;
+        }
+        public string TestCreateArticleVideoUrlType2()
+        {
+            string value = "https://m.youtube.com/watch?v=LSm3SB19fJs";
+            return value;
+        }
+        public string TestCreateArticleVideoUrlType3()
+        {
+            string value = "https://www.youtube.com/watch?v=H34CDpN1pXQ";
+            return value;
+        }
+        public string TestCreateArticleVideoUrlType4()
+        {
+            string value = "https://youtu.be/H34CDpN1pXQ";
+            return value;
+        }
+        public string TestCreateArticleVideoUrlInvalidType1() //Should Autocorrect
+        {
+            string value = "http://www.youtube.com/watch?v=H34CDpN1pXQ";
+            return value;
+        }
+        public string TestCreateArticleVideoUrlInvalidType2() //Should Autocorrect
+        {
+            string value = "www.youtube.com/watch?v=H34CDpN1pXQ";
+            return value;
+        }
+        public string TestCreateArticleVideoUrlInvalidType3() //Should Autocorrect
+        {
+            string value = "https://youtube.com/watch?v=H34CDpN1pXQ";
+            return value;
+        }
+        public string TestCreateArticleVideoUrlInvalidType4() //Should Autocorrect
+        {
+            string value = "youtube.com/watch?v=H34CDpN1pXQ";
+            return value;
+        }
+        public string TestCreateArticleVideoUrlInvalidType5() //Should NOT Autocorrect - Display validation
+        {
+            string value = "http://www.youtube.com/watch?v=H34CDpN1pX";
+            return value;
+        }
+    }
+}
